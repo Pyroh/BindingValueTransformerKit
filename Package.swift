f5f5ccc8cd8654a3ee01020e40327e79f025561d
Pyroh <pyroh@icloud.com>
2021-12-22 18:33:29 +0100

    [FIX] Dependency

--- Package.swift
+++ Package.swift
@@ -24,6 +24,6 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "BindingValueTransformerKit",
-            dependencies: ["OptionalType"])
+            dependencies: ["OptionalType", "ZeroableProtocol"])
     ]
 )
